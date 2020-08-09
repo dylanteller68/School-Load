4fd160369e9236ffe770149b12da839992eb5404
dylanteller68 <dylanteller64@gmail.com>
2020-08-09 12:54:15 -0500

    Added delete account functionality

--- School Load/HelpViewController.swift
+++ School Load/HelpViewController.swift
@@ -10,16 +10,22 @@ import UIKit
 
 class HelpViewController: UIViewController {
 	
+	@IBOutlet weak var delete_acct_btn: UIButton!
 	@IBOutlet weak var scrollview: UIScrollView!
 	
 	var isContactScreen = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
+		delete_acct_btn.layer.cornerRadius = 25
 		if isContactScreen {
 			scrollview.contentOffset = CGPoint(x: 0, y: scrollview.contentSize.height - scrollview.bounds.size.height + scrollview.contentInset.bottom)
 		}
     }
+	
+	@IBAction func delete_acct_tapped(_ sender: Any) {
+		
+	}
 	
 	override func viewDidAppear(_ animated: Bool) {
 		if isContactScreen {
---
+++ School Load/ReauthenticateDeleteAcctViewController.swift
@@ -0,0 +1,119 @@
+//
+//  ReauthenticateDeleteAcctViewController.swift
+//  School Load
+//
+//  Created by Dylan Teller on 8/9/20.
+//  Copyright © 2020 Dylan Teller. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+
+class ReauthenticateDeleteAcctViewController: UIViewController {
+
+	@IBOutlet weak var btn_width_constraint: NSLayoutConstraint!
+	@IBOutlet weak var email_txtbx: UITextField!
+	@IBOutlet weak var password_txtbx: UITextField!
+	@IBOutlet weak var error_lbl: UILabel!
+	@IBOutlet weak var SV_width_constraint: NSLayoutConstraint!
+	@IBOutlet weak var progress_spinner: UIActivityIndicatorView!
+	@IBOutlet weak var verify_btn: UIButton!
+	
+	override func viewDidLoad() {
+        super.viewDidLoad()
+
+		verify_btn.layer.cornerRadius = 25
+		
+		if UIDevice().model == "iPad" {
+			btn_width_constraint.constant += 225
+			SV_width_constraint.constant += 225
+		}
+    }
+
+	override func viewDidAppear(_ animated: Bool) {
+		email_txtbx.becomeFirstResponder()
+	}
+	
+	@IBAction func verify_tapped(_ sender: Any) {
+		email_txtbx.resignFirstResponder()
+		password_txtbx.resignFirstResponder()
+		
+		let email = email_txtbx.text?.trimmingCharacters(in: .whitespacesAndNewlines).lowercased() ?? ""
+		let password = password_txtbx.text ?? ""
+		
+		if email != "" && password != "" {
+			
+			verify_btn.isEnabled = false
+			
+			verify_btn.setTitle("", for: .normal)
+			progress_spinner.isHidden = false
+			progress_spinner.startAnimating()
+			
+			let cred = EmailAuthProvider.credential(withEmail: email, password: password)
+	
+			Auth.auth().currentUser?.reauthenticate(with: cred, completion: { (adr, error) in
+				if error == nil {
+					self.error_lbl.isHidden = true
+					self.progress_spinner.stopAnimating()
+					
+					let notificationFeedbackGenerator = UINotificationFeedbackGenerator()
+					notificationFeedbackGenerator.prepare()
+					notificationFeedbackGenerator.notificationOccurred(.success)
+					
+					self.verify_btn.setTitle("Verified", for: .normal)
+					DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+						self.alert_delete()
+					}
+				} else {
+					let notificationFeedbackGenerator = UINotificationFeedbackGenerator()
+					notificationFeedbackGenerator.prepare()
+					notificationFeedbackGenerator.notificationOccurred(.error)
+					
+					self.error_lbl.text = "Invalid email/password"
+					self.error_lbl.isHidden = false
+					self.progress_spinner.stopAnimating()
+					self.verify_btn.setTitle("Verify", for: .normal)
+					self.verify_btn.isEnabled = true
+				}
+			})
+		} else {
+			let notificationFeedbackGenerator = UINotificationFeedbackGenerator()
+			notificationFeedbackGenerator.prepare()
+			notificationFeedbackGenerator.notificationOccurred(.error)
+			
+			error_lbl.text = "Email/Password required"
+			error_lbl.isHidden = false
+			self.verify_btn.isEnabled = true
+		}
+	}
+	
+	@IBAction func cancel_tapped(_ sender: Any) {
+		self.dismiss(animated: true, completion: nil)
+	}
+	
+	@IBAction func password_done(_ sender: Any) {
+		password_txtbx.resignFirstResponder()
+		verify_tapped(self)
+	}
+	
+	@IBAction func forgot_tapped(_ sender: Any) {
+		let selectionFeedbackGenerator = UISelectionFeedbackGenerator()
+		selectionFeedbackGenerator.selectionChanged()
+	}
+	
+	@IBAction func email_done(_ sender: Any) {
+		email_txtbx.resignFirstResponder()
+	}
+	
+	func alert_delete() {
+		let alert = UIAlertController(title: "Delete Account", message: "Your account and all data will be permanently deleted. This cannot be undone.", preferredStyle: .alert)
+		alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { (action) in	}))
+		alert.addAction(UIAlertAction(title: "Delete", style: .destructive, handler: { (action) in
+			db.collection("users").document(user.ID).delete()
+			Auth.auth().currentUser?.delete()
+			self.performSegue(withIdentifier: "delete_acct_segue", sender: nil)
+		}))
+		
+		present(alert, animated: true)
+	}
+}

79cd6dd28c4f40fcc205f8aa00dc614f0dfe25f0
dylanteller68 <dylanteller64@gmail.com>
2020-08-09 09:48:47 -0500

    Initializing help screen

---
+++ School Load/HelpViewController.swift
@@ -0,0 +1,21 @@
+//
+//  HelpViewController.swift
+//  School Load
+//
+//  Created by Dylan Teller on 8/9/20.
+//  Copyright © 2020 Dylan Teller. All rights reserved.
+//
+
+import UIKit
+
+class HelpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+	
+	@IBAction func cancel_tapped(_ sender: Any) {
+		self.dismiss(animated: true, completion: nil)
+	}
+}

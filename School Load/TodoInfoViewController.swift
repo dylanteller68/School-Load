86b79d4fddc600cfc59d207bb73c52ff6cb54897
dylanteller68 <dylanteller64@gmail.com>
2020-08-11 17:52:18 -0500

    Initializing todo info screen

---
+++ School Load/TodoInfoViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TodoInfoViewController.swift
+//  School Load
+//
+//  Created by Dylan Teller on 8/11/20.
+//  Copyright © 2020 Dylan Teller. All rights reserved.
+//
+
+import UIKit
+
+class TodoInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
